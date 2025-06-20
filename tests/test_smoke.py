6b3a841b2be9f8fdaf38ac0b178cc57a8d2b9d06
spells39 <spells228@gmail.com>
2025-06-20 16:07:10 +0300

    init

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -3,12 +3,11 @@ from starlette.status import HTTP_200_OK
 
 from app.models.models import Status
 
-class SmokeTest:
-    def test_check_status(self, app_url):
-        resp = requests.get(f"{app_url}/api/check_status")
-        assert resp.status_code == HTTP_200_OK
+def test_check_status(app_url):
+    resp = requests.get(f"{app_url}/api/check_status")
+    assert resp.status_code == HTTP_200_OK
 
-    def test_check_status_users(self, app_url):
-        resp = requests.get(f"{app_url}/api/check_status")
-        assert resp.status_code == HTTP_200_OK
-        assert Status.parse_raw(resp.text).database
+def test_check_status_users(app_url):
+    resp = requests.get(f"{app_url}/api/check_status")
+    assert resp.status_code == HTTP_200_OK
+    assert Status.parse_raw(resp.text).database
